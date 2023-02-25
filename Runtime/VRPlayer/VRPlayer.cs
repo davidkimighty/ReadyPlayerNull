d392a2b37ad97d88d3ccbd59aadcc9940c15f846
David <davidkimighty@gmail.com>
2023-02-25 23:05:18 +0900

    #1 basic locomotion

---
+++ Runtime/VRPlayer/VRPlayer.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+namespace CollieMollie.XR.Player
+{
+    public class VRPlayer : MonoBehaviour
+    {
+        #region Variable Field
+        [Header("Locomotion Providers")]
+        [SerializeField] private ActionBasedContinuousMoveProvider _continuousMoveProvider = null;
+        [SerializeField] private ActionBasedSnapTurnProvider _snapTurnProvider = null;
+        [SerializeField] private ActionBasedContinuousTurnProvider _continuousTurnProvider = null;
+        [SerializeField] private TeleportationProvider _teleportationProvider = null;
+
+        #endregion
+    }
+}
